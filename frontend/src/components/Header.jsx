7133027366e1f400eecbc697b08c34413d59c9be
JackPangalia <jackpangalia1@gmail.com>
2025-03-02 21:16:39 -0800

    cahnged the intro text on the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = ({ isExpanded, setIsExpanded, hasMessages, onClearChat }) => {
     <div className="flex items-center justify-between p-3 border-b border-gray-200 rounded-tr-2xl rounded-tl-2xl">
       <div className="flex items-center">
         {/* <img src="././dist/logo_placeholder.jpg" alt="logo" className="w-auto h-6" /> */}
-        <p className="ml-2">LearnAI</p>
+        <p className="ml-2">Learn about us with AI</p>
       </div>
       <div className="flex items-center space-x-2">
         {/* Close button */}
